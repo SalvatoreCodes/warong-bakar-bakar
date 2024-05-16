d05ff4850424807833f25c04a3646653d8b446f0
SalvatoreCodes <goniputra1@gmail.com>
2024-05-16 08:50:59 +0800

    16/5/2024

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="card--container">
+      <div className="card">
+        <div className="card--image"></div>
+        <div className="card--information"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
